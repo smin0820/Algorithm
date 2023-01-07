a2e5709d760626d9c90b36f97a8715db17e4a109
신민 <smin_0820@naver.com>
2023-01-07 11:24:49 +0900

    두 수를 비교한 결과를 출력하는 문제

---
+++ BOJ/1330.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const input = fs.readFileSync('dev/stdin').toString().split(' ');
+
+const A = Number(input[0]);
+const B = Number(input[1]);
+
+
+if (A > B) {
+    console.log('>');
+} else if (A < B) {
+    console.log('<');
+} else {
+    console.log('==');
+}
