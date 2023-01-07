a2f229ef474316b7cf496c52cbf0a0f9fa3dfe6c
신민 <smin_0820@naver.com>
2023-01-07 11:25:01 +0900

    시험 점수를 성적으로 바꾸는 문제

---
+++ BOJ/9498.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const score = fs.readFileSync('dev/stdin').toString();
+
+if (score >= 90 && score <= 100) {
+    console.log('A');
+} else if (score >= 80 && score <= 89) {
+    console.log('B');
+} else if (score >= 70 && score <= 79) {
+    console.log('C');
+} else if (score >= 60 && score <= 69) {
+    console.log('D');
+} else {
+    console.log('F');
+}
