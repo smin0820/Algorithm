b6bafbdce6fa7bc782e25d1ccfd589a75a94b9e3
민 <97029953+smin0820@users.noreply.github.com>
2023-05-26 15:22:52 +0900

    Delete Programmers directory

--- Programmers/배열 두배 만들기.js
+++
@@ -1,4 +0,0 @@
-function solution(numbers) {
-    var answer = numbers.map((e) => e * 2);
-    return answer;
-}
--- Programmers/중앙값 구하기.js
+++
@@ -1,4 +0,0 @@
-function solution(array) {
-    var answer = array.sort((a, b) => a -b);
-    return answer[Math.floor(answer.length / 2)];
-}
