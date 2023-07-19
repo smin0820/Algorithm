2dd44558aa72dc934e6ca67fe85a88ae5659dc58
smin0820 <97029953+smin0820@users.noreply.github.com>
2023-07-19 23:51:35 +0900

    가운데 글자 가져오기.js

---
+++ Programmers/level 1/가운데 글자 가져오기.js
@@ -0,0 +1,6 @@
+function solution(s) {
+    return s.length % 2 === 0 ? s[s.length / 2 - 1] + s[s.length / 2] : s[Math.floor(s.length / 2)];
+}
+
+console.log(solution("abcde"));
+console.log(solution("qwer"));
