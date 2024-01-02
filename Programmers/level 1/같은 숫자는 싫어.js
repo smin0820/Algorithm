ecb0ac5b3494d5f1e96f88658758b7a54e60ee35
smin0820 <97029953+smin0820@users.noreply.github.com>
2024-01-02 18:54:54 +0900

    같은 숫자는 싫어.js

---
+++ Programmers/level 1/같은 숫자는 싫어.js
@@ -0,0 +1,12 @@
+function solution(arr)
+{
+    var answer = [];
+    for(i = 0; i < arr.length; i++) {
+        if(arr[i] !== arr[i + 1]) {
+            answer.push(arr[i]);
+        }
+    }
+    console.log(answer)
+    
+    return answer;
+}
