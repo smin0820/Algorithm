08e43732aaecae6f0652b9620e5df9415f603558
smin0820 <97029953+smin0820@users.noreply.github.com>
2024-02-13 20:39:49 +0900

    푸드 파이트 대회.js

---
+++ Programmers/level 1/푸드 파이트 대회.js
@@ -0,0 +1,7 @@
+function solution(food) {
+    var answer = '';
+    for(i = 1; i < food.length; i++) {
+        answer += String(i).repeat(Math.floor(food[i] / 2));
+    }
+    return answer + '0' + [...answer].reverse().join('');
+}
