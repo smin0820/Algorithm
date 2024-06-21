ced0357c492396e1c85b53c6b9502aa7733f16a2
smin0820 <97029953+smin0820@users.noreply.github.com>
2024-06-21 21:09:21 +0900

    11650.js

--- BOJ/11650.js
+++ BOJ/11650.js
@@ -1,10 +1,26 @@
 const fs = require('fs');
-const input = fs.readFileSync('./input.txt').toString().trim().split('\n');
-input.shift();
-const arr = [];
-for (i = 0; i < input.length; i++) {
-    arr.push(input[i].split(' ').map(Number));
+const input = fs.readFileSync('input.txt').toString().split('\n');
+
+let n = Number(input[0]);
+let data = [];
+for(let i = 1; i <= n; i++) {
+    data.push(input[i].split(' ').map(Number));
 }
-let result = '';
-arr.sort((a, b) => a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]).forEach(e => result += `${e[0]} ${e[1]}\n`);
-console.log(result);
+
+data.sort((a, b) => a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]);
+
+let answer = "";
+for(let point of data) {
+    answer += point[0] + " " + point[1] + "\n";
+}
+
+console.log(answer);
+
+// input.shift();
+// const arr = [];
+// for (i = 0; i < input.length; i++) {
+//     arr.push(input[i].split(' ').map(Number));
+// }
+// let result = '';
+// arr.sort((a, b) => a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]).forEach(e => result += `${e[0]} ${e[1]}\n`);
+// console.log(result);
