3f2b4e86b75d79f64e445b5b68f3cb7841fba047
smin0820 <97029953+smin0820@users.noreply.github.com>
2024-07-07 17:43:47 +0900

    1654.js

---
+++ BOJ/1654.js
@@ -0,0 +1,32 @@
+let fs = require('fs');
+let input = fs.readFileSync('input.txt').toString().split('\n');
+
+let k = Number(input[0].split(' ')[0]);
+let n = Number(input[0].split(' ')[1]);
+
+let arr = [];
+for(let i = 1; i <= k; i++) {
+    arr.push(Number(input[i]));
+}
+arr.sort((a, b) => a - b);
+let start = 1;
+let end = arr.reduce((a, b) => Math.max(a, b));
+
+let result = 0;
+while(start <= end) {
+    let mid = parseInt((start + end) / 2);
+    let total = 0;
+    for(let x of arr) {
+        if(total < n) {
+            total += parseInt(x / mid);
+        }
+    } 
+    if(total < n) {
+        end = mid - 1;
+    } else {
+        result = mid;
+        start = mid + 1;
+    }
+}
+
+console.log(result);
