3c961155577bb4f13daf27109934bf8fe1d3e6e6
smin0820 <97029953+smin0820@users.noreply.github.com>
2024-07-27 08:23:25 +0900

    10974.js

---
+++ BOJ/10974.js
@@ -0,0 +1,31 @@
+let fs = require('fs');
+let input = fs.readFileSync('input.txt').toString().split(' ');
+let n = Number(input[0]);
+
+let arr = [];
+for(let i = 1; i <= n; i++) arr.push(i);
+let visited = new Array(n).fill(false);
+let selected = [];
+
+let answer = '';
+function dfs(arr, depth) {
+    if(depth === n) {
+        let result = [];
+        for(let x of selected) result.push(x);
+        for(let y of result) answer += y + ' ';
+        answer += '\n';
+        return;  
+    }
+    for(let i = 1; i <= arr.length; i++) {
+        if(visited[i]) continue;
+        selected.push(i);
+        visited[i] = true;
+        dfs(arr, depth + 1);
+        selected.pop();
+        visited[i] = false; 
+    }
+}
+
+dfs(arr, 0);
+console.log(answer);
+
