e000015d5bdeaf283b83a7d8a89e225138916bf5
smin0820 <97029953+smin0820@users.noreply.github.com>
2024-08-01 00:08:20 +0900

    15651.js

---
+++ BOJ/15651.js
@@ -0,0 +1,28 @@
+let fs = require('fs');
+let input = fs.readFileSync('input.txt').toString().split(' ');
+
+let n = Number(input[0]);
+let m = Number(input[1]);
+
+let arr = [];
+for(let i = 1; i <= n; i++) arr.push(i);
+let selected = [];
+
+let answer = '';
+function dfs(arr, depth) {
+    if(depth === m) {
+        let result = [];
+        for(let x of selected) result.push(arr[x]);
+        for(let y of result) answer += y + ' ';
+        answer += '\n';
+        return;
+    }
+    for(let i = 0; i < arr.length; i++) {
+        selected.push(i);
+        dfs(arr, depth + 1);
+        selected.pop();
+    }
+}
+
+dfs(arr, 0);
+console.log(answer);
