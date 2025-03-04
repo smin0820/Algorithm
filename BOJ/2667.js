a3f7bc9e8f2d5df10ab20848b2b5f40a4a59970f
smin0820 <97029953+smin0820@users.noreply.github.com>
2025-03-04 17:28:12 +0900

    2667.js

--- BOJ/2667.js
+++ BOJ/2667.js
@@ -2,27 +2,27 @@ const fs = require('fs');
 const input = fs.readFileSync('input.txt').toString().trim().split('\n');
 
 const n = Number(input[0]);
-const graph = [];
+let graph = [];
 
 for(let i = 1; i <= n; i++) {
     graph.push(input[i].trim().split('').map(Number));
 }
 
+let total = 0;
 let answer = [];
-
 for(let i = 0; i < n; i++) {
     for(let j = 0; j < n; j++) {
-        let total = dfs(i, j);
+        total = dfs(i, j);
         if(total > 0) answer.push(total);
     }
 }
 
 function dfs(x, y) {
-    if(x <= -1 || y <= -1 || x >= n || y >= n) {
+    if(x >= n || x <= -1 || y <= -1 || y >= n) {
         return 0;
     }
-
-    if(1 <= graph[x][y]) {
+ 
+    if(graph[x][y] === 1) {
         graph[x][y] = -1;
         let result = 1;
         result += dfs(x, y + 1);
@@ -31,11 +31,86 @@ function dfs(x, y) {
         result += dfs(x - 1, y);
         return result;
     }
-
     return 0;
 }
-
 answer.sort((a, b) => a - b);
-console.log(`${answer.length}\n${answer.join('\n')}`);
+
+console.log(answer.length);
+console.log(answer.join('\n'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const n = Number(input[0]);
+// const graph = [];
+
+// for(let i = 1; i <= n; i++) {
+//     graph.push(input[i].trim().split('').map(Number));
+// }
+
+// let answer = [];
+
+// for(let i = 0; i < n; i++) {
+//     for(let j = 0; j < n; j++) {
+//         let total = dfs(i, j);
+//         if(total > 0) answer.push(total);
+//     }
+// }
+
+// function dfs(x, y) {
+//     if(x <= -1 || y <= -1 || x >= n || y >= n) {
+//         return 0;
+//     }
+
+//     if(1 <= graph[x][y]) {
+//         graph[x][y] = -1;
+//         let result = 1;
+//         result += dfs(x, y + 1);
+//         result += dfs(x, y - 1);
+//         result += dfs(x + 1, y);
+//         result += dfs(x - 1, y);
+//         return result;
+//     }
+
+//     return 0;
+// }
+
+// answer.sort((a, b) => a - b);
+// console.log(`${answer.length}\n${answer.join('\n')}`);
 
 
