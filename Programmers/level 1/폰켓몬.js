c54c4e941d3ceed2846cc401a429f968c8c2aa58
smin0820 <97029953+smin0820@users.noreply.github.com>
2026-08-13 21:05:20 +0900

    Update 폰켓몬.js

--- Programmers/level 1/폰켓몬.js
+++ Programmers/level 1/폰켓몬.js
@@ -1,5 +1,5 @@
 function solution(nums) {
-    let select = nums.length / 2;
-    let set = new Set(nums);
-    return set.size > select ? select : set.size;
+  let select = nums.length / 2;
+  let set = new Set(nums);
+  return set.size > select ? select : set.size;
 }
